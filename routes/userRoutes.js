8746aa70775b36f45c59e5d593ee9b2c6bd65215
Raghav Gandhi <imraghavgandhi@gmail.com>
2024-07-29 17:29:15 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const userController = require('../controllers/userController');
-const { signupValidation, loginValidation, validate } = require('../middleware/userValidation');
 const router = express.Router();
+const { signupValidation, loginValidation, validate } = require('../middleware/userValidation');
+const userController = require('../controllers/userController');
 
-// Signup Route
 router.post('/signup', signupValidation, validate, userController.signup);
-
-// Login Route
 router.post('/login', loginValidation, validate, userController.login);
 
 module.exports = router;

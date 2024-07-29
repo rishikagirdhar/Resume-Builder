298d0b824409d3df70a0235f6726b4e138e85ebd
Raghav Gandhi <imraghavgandhi@gmail.com>
2024-07-29 17:29:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-// Function to connect to MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true, // Use the new URL parser
-      useUnifiedTopology: true, // Use the new Server Discover and Monitoring engine
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB connected');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
     process.exit(1); // Exit process with failure

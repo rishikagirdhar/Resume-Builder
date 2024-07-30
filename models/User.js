2f304caca9370bd155ae352d508358ad7ee6716a
Raghav Gandhi <imraghavgandhi@gmail.com>
2024-07-30 12:39:35 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
 const bcrypt = require('bcryptjs');
-
+ 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -24,27 +24,38 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true,
 });
-
-// Hash password before saving
+ 
+// Virtual field for confirm password
+userSchema.virtual('confirmPassword')
+  .get(function() {
+    return this._confirmPassword;
+  })
+  .set(function(value) {
+    this._confirmPassword = value;
+  });
+ 
+// Pre-save middleware to check if passwords match
 userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    return next();
+  if (this.isModified('password')) {
+    if (this.password !== this.confirmPassword) {
+      this.invalidate('confirmPassword', 'Passwords do not match');
+    }
+ 
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (err) {
+      return next(err);
+    }
   }
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  next();
 });
-
+ 
 // Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
-
+ 
 const User = mongoose.model('User', userSchema);
-
+ 
 module.exports = User;
